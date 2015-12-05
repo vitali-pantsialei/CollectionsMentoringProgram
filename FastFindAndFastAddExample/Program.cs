6d594e46771574c39da524f268d9f2107cfb05cc
Vitali Pantsialei <bitvitalby@gmail.com>
2015-12-05 20:31:08 +0300

    add collections task

---
+++ FastFindAndFastAddExample/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFindAndFastAddExample
+{
+    public class Program
+    {
+        private const int ElementsCount = 20000000;
+        private const int RemoveElement = 77777;
+        static void Main(string[] args)
+        {
+            string[] data = new string[ElementsCount];
+            Console.WriteLine("Please wait till all {0} will be initialized...", ElementsCount);
+            for (int i = 0; i != ElementsCount; i++)
+                data[i] = GenerateString(3, 20);
+
+            //HashSet обеспечит максимальную скорость поиска и удаления элемента,
+            //так как определяет индекс где хранить элемент по хэш коду объекта 
+            HashSet<string> hashSet = new HashSet<string>();
+
+            Console.WriteLine("Initialize HashSet by {0} elements", ElementsCount);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i != ElementsCount; i++)
+                hashSet.Add(data[i]);
+            stopWatch.Stop();
+            Console.WriteLine("Initialize complete. " + GetTimeString(stopWatch));
+
+            Console.WriteLine("Find and remove one element in HashSet");
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+            hashSet.Remove(data[RemoveElement]);
+            stopWatch.Stop();
+            Console.WriteLine("Find and remove complete. " + GetTimeString(stopWatch));
+            
+            //List обеспечивает быстрое добавление элементов для хранения,
+            //так как при добавлении элемента он просто добавляет его в конец списка
+            List<string> listExample = new List<string>();
+            Console.WriteLine("Initialize List by {0} elements", ElementsCount);
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i != ElementsCount; i++)
+                listExample.Add(data[i]);
+            stopWatch.Stop();
+            Console.WriteLine("Initialize complete. " + GetTimeString(stopWatch));
+
+            Console.WriteLine("Find and remove one element in a List");
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+            listExample.Remove(data[RemoveElement]);
+            stopWatch.Stop();
+            Console.WriteLine("Find and remove complete. " + GetTimeString(stopWatch));
+        }
+
+        public static string GetTimeString(Stopwatch stopWatch)
+        {
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            return "RunTime " + elapsedTime;
+        }
+
+        public static string GenerateString(int minLength, int maxLength)
+        {
+            Random rand = new Random();
+            StringBuilder result = new StringBuilder();
+            int length = rand.Next(minLength, maxLength);
+            for (int i = 0; i != length; i++)
+                result.Append((char)('a' + rand.Next(26)));
+            return result.ToString();
+        }
+    }
+}
---
+++ FirstAndFastUnitTest/FindAnsFastUnitTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using FastFindAndFastAddExample;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace FirstAndFastUnitTest
+{
+    [TestClass]
+    public class FindAnsFastUnitTests
+    {
+        private const int ElementsCount = 10000000;
+        private const int RemoveElement = 77777;
+        private string[] data;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            data = new string[ElementsCount];
+            for (int i = 0; i != ElementsCount; i++)
+                data[i] = Program.GenerateString(3, 20);
+        }
+
+        [TestMethod]
+        public void Test_HashSet_FindAndDeleteElement()
+        {
+            //Arrange
+            HashSet<string> hashSet = new HashSet<string>(data);
+            Stopwatch stopWatch = new Stopwatch();
+
+            //Act
+            stopWatch.Start();
+            hashSet.Remove(data[RemoveElement]);
+            stopWatch.Stop();
+
+            //Assert
+            long result = stopWatch.ElapsedMilliseconds;
+            long lessThanMs = 1;
+
+            Assert.IsTrue(result < lessThanMs);
+        }
+
+        [TestMethod]
+        public void Test_List_AddElement()
+        {
+            //Arrange
+            List<string> list = new List<string>();
+            Stopwatch stopWatch = new Stopwatch();
+            for (int i = 0; i != ElementsCount - 1; i++)
+                list.Add(data[i]);
+
+            //Act
+            stopWatch.Start();
+            list.Add(data[ElementsCount - 1]);
+            stopWatch.Stop();
+
+            //Assert
+            long result = stopWatch.ElapsedMilliseconds;
+            long lessThanMs = 1;
+
+            Assert.IsTrue(result < lessThanMs);
+        }
+    }
+}
